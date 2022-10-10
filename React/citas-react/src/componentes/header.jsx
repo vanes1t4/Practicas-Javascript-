6a60fbe38c4fe310167efa6b003e9a5adbc0b746
Vanesa <mvanerodriguez@gmail.com>
2022-10-10 11:57:31 -0500

    Aplicando el H2 Listado
    Se agrega H2 Listado pacientes sin funcionar

---
+++ React/citas-react/src/componentes/header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+        <h1>Hola, es mi primer componente</h1>
+    )
+}
+
+export default Header
---
+++ React/citas-react/src/componentes/listadoPaciente.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const listadoPacientes= ()=>{
+    return(
+        <h2>Listado</h2>
+    )
+}
+
+export default listadoPacientes
+
